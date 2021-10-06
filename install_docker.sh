640ddff58da84b29399ee5bc0e9bf1814521490d
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-06 10:34:55 -0400

    add error and check_internet functions to installer

--- install_docker.sh
+++ install_docker.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
-curl -sSL https://get.docker.com | sh
-sudo usermod -aG docker $USER
-echo "remember to logoff to take effect"
+
+function error {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+function check_internet() {
+  printf "Checking if you are online..."
+  wget -q --spider http://github.com
+  if [ $? -eq 0 ]; then
+    echo "Online. Continuing."
+  else
+    error "Offline. Go connect to the internet then run the script again."
+  fi
+}
+
+check_internet
+
+curl -sSL https://get.docker.com | sh || error "Failed to install Docker."
+sudo usermod -aG docker $USER || error "Failed to add user to the Docker usergroup."
+echo "Remember to logoff/reboot for the changes to take effect."
