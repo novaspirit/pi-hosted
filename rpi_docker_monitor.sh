58772d44ce0e8e9b63f0b0d660d7e3877f61bdb2
novaspirit <novaspirit@novaspirit.com>
2021-10-07 21:28:47 -0400

    fix typo

--- rpi_docker_monitor.sh
+++ rpi_docker_monitor.sh
@@ -4,7 +4,7 @@ sudo mkdir -p /portainer/Files/AppData/Config/prometheus/config
 sudo mkdir -p /portainer/Files/AppData/Config/prometheus/data
 sudo mkdir -p /portainer/Files/AppData/Config/grafana/data
 echo "downloading prometheus config files"
-sudo wget -o /portainer/Files/AppData/Config/prometheus/config/prometheus.yml https://raw.githubusercontent.com/oijkn/Docker-Raspberry-PI-Monitoring/main/prometheus/prometheus.yml
+sudo wget -O /portainer/Files/AppData/Config/prometheus/config/prometheus.yml https://raw.githubusercontent.com/oijkn/Docker-Raspberry-PI-Monitoring/main/prometheus/prometheus.yml
 sudo touch /portainer/Files/AppData/Config/grafana/grafana.ini
 echo "setting permissions"
 sudo chown -R 472:472 /portainer/Files/AppData/Config/grafana/data
