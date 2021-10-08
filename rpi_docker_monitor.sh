9c73d37b049c3473a34d14287b58fb7d9decbaa7
novaspirit <novaspirit@novaspirit.com>
2021-10-07 21:24:27 -0400

    added script and config files for raspberry pi docker monitor

---
+++ rpi_docker_monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "creating directories"
+sudo mkdir -p /portainer/Files/AppData/Config/prometheus/config
+sudo mkdir -p /portainer/Files/AppData/Config/prometheus/data
+sudo mkdir -p /portainer/Files/AppData/Config/grafana/data
+echo "downloading prometheus config files"
+sudo wget -o /portainer/Files/AppData/Config/prometheus/config/prometheus.yml https://raw.githubusercontent.com/oijkn/Docker-Raspberry-PI-Monitoring/main/prometheus/prometheus.yml
+sudo touch /portainer/Files/AppData/Config/grafana/grafana.ini
+echo "setting permissions"
+sudo chown -R 472:472 /portainer/Files/AppData/Config/grafana/data
+echo "done"
