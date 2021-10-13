bc7323fe8e129f098e11e354d8ed2c07327f5f2f
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-13 07:19:55 -0400

    Update rpi_docker_monitor.sh

--- rpi_docker_monitor.sh
+++ rpi_docker_monitor.sh
@@ -18,10 +18,10 @@ function check_internet() {
 check_internet
 
 echo "Creating directories..."
-sudo mkdir -p /portainer/Files/AppData/Config/prometheus/config
-sudo mkdir -p /portainer/Files/AppData/Config/prometheus/data
-sudo mkdir -p /portainer/Files/AppData/Config/grafana/data
-echo "downloading prometheus config files"
+sudo mkdir -p /portainer/Files/AppData/Config/prometheus/config || error "Failed to create config directory!"
+sudo mkdir -p /portainer/Files/AppData/Config/prometheus/data || error "Failed to create data directory for Prometheus!"
+sudo mkdir -p /portainer/Files/AppData/Config/grafana/data || error "Failed to create data directory for Grafana!"
+echo "Downloading Prometheus config files"
 sudo wget -O /portainer/Files/AppData/Config/prometheus/config/prometheus.yml https://raw.githubusercontent.com/oijkn/Docker-Raspberry-PI-Monitoring/main/prometheus/prometheus.yml || error "Failed to download prometheus.yml file!"
 sudo touch /portainer/Files/AppData/Config/grafana/grafana.ini || error "Failed to touch grafana.ini file!"
 echo "Setting permissions..."
