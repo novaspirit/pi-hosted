743dad22f7f7d824c43c55ad4f2b38e629815a13
Robert Middleswarth <robert@middleswarth.net>
2021-10-27 20:10:23 -0400

    Rename the update script as this is needed for more than just tautulli on PI's but not needed for ubuntu.

--- rpi_tautulli_update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ~
-wget http://ftp.us.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.5.2-2_armhf.deb
-sudo dpkg -i libseccomp2_2.5.2-2_armhf.deb
-rm -f libseccomp2_2.5.2-2_armhf.deb
-echo "Updating the libseccomp file is complete, you may now install Tautulli without issue."
