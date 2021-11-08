dc7966fbdc4cf512419275bbc52f116a7e7f8cb8
Robert Middleswarth <robert@middleswarth.net>
2021-11-08 09:48:34 -0500

    Update Throttled install script second draft

--- tools/install_throttled.sh
+++ tools/install_throttled.sh
@@ -20,6 +20,6 @@ check_internet
 echo "Downloading throttled.sh script"
 sudo wget -O /usr/local/bin/throttled.sh https://gist.githubusercontent.com/aallan/0b03f5dcc65756dde6045c6e96c26459/raw/c0e75e2599d8ed76f834d44bc3ce84e0f5f2f4bc/throttled.sh || error "Failed to download throttled.sh file!"
 echo "Setting permissions..."
-sudo chmod -R 755:755 /usr/local/bin/throttled.sh || error "Failed to set permissions for throttled.sh!"
+sudo chmod -R 755 /usr/local/bin/throttled.sh || error "Failed to set permissions for throttled.sh!"
 echo "First Run of Throttled.sh..."
-/usr/local/bin/throttled.sh
+sudo /usr/local/bin/throttled.sh
