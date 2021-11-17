9e138581be3127539e5be60866ee940110dab05b
Eduardo R <eduardo.rubio.garcia@gmail.com>
2021-11-17 13:56:56 -0800

    added 2 spaces to docker sh

--- install_docker.sh
+++ install_docker.sh
@@ -20,3 +20,4 @@ check_internet
 curl -sSL https://get.docker.com | sh || error "Failed to install Docker."
 sudo usermod -aG docker $USER || error "Failed to add user to the Docker usergroup."
 echo "Remember to logoff/reboot for the changes to take effect."
+
