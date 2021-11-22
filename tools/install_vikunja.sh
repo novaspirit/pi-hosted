b2b424c50c5dda6864731fa86cd0297857242542
Robert Middleswarth <72569+rmiddle@users.noreply.github.com>
2021-11-21 23:44:36 -0500

    Add in proxy support to see if we can get the frontend working correctly. (#198)

---
+++ tools/install_vikunja.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function error {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+function check_internet() {
+  printf "Checking if you are online..."
+  wget -q --spider http://github.com
+  if [ $? -eq 0 ]; then
+    echo "Online. Continuing."
+  else
+    error "Offline. Go connect to the internet then run the script again."
+  fi
+}
+
+check_internet
+
+echo "Downloading vikunja config files"
+sudo wget -O /portainer/Files/AppData/Config/Vikunja/nginx.conf https://raw.githubusercontent.com/novaspirit/pi-hosted/master/configs/vikunja_nginx.conf || error "Failed to download nginx.conf file!"
+echo "Done You are ready to install the Vikunja Template"
