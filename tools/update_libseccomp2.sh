f46096619d449f7696b21e7378673a0dc1366c0b
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-11-28 20:35:44 -0500

    Update update_libseccomp2.sh

--- tools/update_libseccomp2.sh
+++ tools/update_libseccomp2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Libseccomp2 upgrade needed if you run rpi 32bit. Check version dpkg-query -W libseccomp, if the version 2.3 or blow, please run this script."
+echo "Libseccomp2 upgrade needed if you run rpi 32bit. Check version dpkg-query -W libseccomp, if the version 2.3 or below, please run this script."
 cd ~
 wget http://ftp.us.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.5.2-2_armhf.deb
 sudo dpkg -i libseccomp2_2.5.2-2_armhf.deb
