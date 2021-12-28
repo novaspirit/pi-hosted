2551c11c4ccd2f4dfe065f3dfe1d6d1ac6680102
Rafael Blumberg <github-v982ma@r.acmrb.uk>
2021-12-28 14:56:59 +0000

    Allow multiple Extras

--- build/generateList.sh
+++ build/generateList.sh
@@ -64,19 +64,36 @@ do
 
 		# Get Script from app info
 		script=$(echo "$info" | jq ".Script" | tr -d '"')
-		if [ "$script" != "null" ]; then script="[]($Scripts$script)"; else unset script; fi
+		if [ "$script" != "null" ]; then script="**[]($Scripts$script)**"; else unset script; fi
 
 		# Get Script from app info
 		extra=$(echo "$info" | jq ".Extra" | tr -d '"')
-		if [ "$extra" != "null" ]; then extra="[$extra]($Extras$extra)"; else unset extra; fi
+		if [ "$extra" != "null" ]; then
+			# If only one entry
+			if [ "$(echo "$extra" | wc -l )" == "1" ]; then
+				extra="[$extra]($Extras$extra)"
+			
+			# If multiples entries
+			else
+				n_ext=$(echo "$info" | jq '.Extra | length')
+				for n in $(seq 0 $(( n_ext - 1 ))); do
+					ext=$(echo "$info" | jq ".Extra[$n]" | tr -d \")
+					if [ "$n" == "0" ]; then
+						txt="[$ext]($Extras$ext)"
+					else
+						txt="$txt<br>[$ext]($Extras$ext)"
+					fi
+				done
+				extra=$txt
+			fi
+		else
+			unset extra
+		fi
 
 		# Get Script from app info
 		vid=$(echo "$info" | jq ".VideoID" | tr -d '"')
 		if [ "$vid" != "null" ] ; then
-			info=$(jq ".youtube[] | select(.ID==\"$vid\")" "$appinfo")
-			vidURL=$(echo "$info" | jq '.URL' | tr -d '"')
-			#vidTitle=$(echo "$info" | jq '.Title' | tr -d '"')
-			#vid="[$vidTitle]($vidURL)"
+			vidURL=$(jq ".youtube[] | select(.ID==\"$vid\") | .URL " "$appinfo" | tr -d \")
 			vid="[![YouTube](https://img.shields.io/badge/YouTube-FF0000?style=plastic&logo=youtube&logoColor=white)]($vidURL)"
 		else
 			unset vid
