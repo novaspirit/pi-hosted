a98b7666cae1369f3a683abb6112ae6333381070
Robert Middleswarth <robert@middleswarth.net>
2022-05-28 12:37:31 -0400

    Update to use a newer version of libseccomp2 as the old one is no longer avaible.

--- tools/update_libseccomp2.sh
+++ tools/update_libseccomp2.sh
@@ -18,7 +18,7 @@ function check_internet() {
 check_internet
 
 echo "Libseccomp2 upgrade needed if you run rpi 32bit. Check version with 'dpkg-query -W libseccomp'. if the version is 2.3 or below, this script is necessary."
-wget http://ftp.us.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.5.3-2_armhf.deb || error "Failed to download libseccomp2.deb"
-sudo dpkg -i libseccomp2_2.5.3-2_armhf.deb || error "Failed to install libseccomp2.deb"
-rm -f libseccomp2_2.5.3-2_armhf.deb
+wget  http://ftp.us.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.5.4-1_armhf.deb || error "Failed to download libseccomp2.deb"
+sudo dpkg -i libseccomp2_2.5.4-1_armhf.deb || error "Failed to install libseccomp2.deb"
+rm -f libseccomp2_2.5.4-1_armhf.deb
 echo "Updating the libseccomp2 file is complete."
