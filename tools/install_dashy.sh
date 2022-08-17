2386c430e6a388320f04378e681dd146aacf654b
Robert Middleswarth <72569+rmiddle@users.noreply.github.com>
2022-08-17 09:38:01 -0400

    Update install_dashy.sh

--- tools/install_dashy.sh
+++ tools/install_dashy.sh
@@ -8,5 +8,5 @@ function error {
 echo "Creating directory..."
 sudo mkdir -p /portainer/Files/AppData/Config/Dashy/ || error "Failed to create Dashy folder!"
 echo "Creating a blank Dashy config files"
-sudo touch /portainer/Files/AppData/Config/Dashy/local-conf.yml || error "Failed to touch config.json file!"
+sudo touch /portainer/Files/AppData/Config/Dashy/local-conf.yml || error "Failed to touch local-conf.yml file!"
 echo "Setup complete. You can now install the Dashy using the App Template."
