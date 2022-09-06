2360aa8d2e860373ef3dda1bed819bd585d556c7
Robert Middleswarth <robert@middleswarth.net>
2022-09-05 22:54:48 -0400

    Add install script for homer

---
+++ tools/install_homer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function error {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+echo "Creating directory..."
+sudo mkdir -p /portainer/Files/AppData/Config/Homer || error "Failed to create Homer folder!"
+sudo mkdir -p /portainer/Files/AppData/Config/Homer/assets || error "Failed to create Homer folder!"
+sudo chown -R 1000.1000 /portainer/Files/AppData/Config/Homer || error "Failed to create Homer folder!"
+echo "Setup complete. You can now install the Homer using the App Template."
