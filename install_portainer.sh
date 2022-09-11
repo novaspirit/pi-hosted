f47ed31db1bb0e2143d36fd01e57f03a4472641a
Robert Middleswarth <robert@middleswarth.net>
2022-09-11 17:24:10 -0400

    Update the installer

--- install_portainer.sh
+++ install_portainer.sh
@@ -18,5 +18,5 @@ function check_internet() {
 check_internet
 
 sudo docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
-sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
+sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest --logo "https://pi-hosted.com/pi-hosted-logo.png" || error "Failed to run Portainer docker image!"
 
