55de851f7fe9ea71eb67ae62cca223c9365c26ac
Robert Middleswarth <robert@middleswarth.net>
2022-09-15 21:28:01 -0400

    Tweak the install file a bit to make it more smoth of a process.

--- tools/install_dashy.sh
+++ tools/install_dashy.sh
@@ -7,11 +7,10 @@ function error {
 
 echo "Creating directory..."
 sudo mkdir -p /portainer/Files/AppData/Config/Dashy/ || error "Failed to create Dashy folder!"
-echo "Confirm that local-conf.yml is not a directory"
+echo "Creating a blank Dashy config files"
 if [ -d /portainer/Files/AppData/Config/Dashy/local-conf.yml ];
 then
     sudo rm -rf /portainer/Files/AppData/Config/Dashy/local-conf.yml || error "Failed to remove the folder /portainer/Files/AppData/Config/Dashy/local-conf.yml!"
 fi
-echo "Creating a blank Dashy config files"
 sudo touch /portainer/Files/AppData/Config/Dashy/local-conf.yml || error "Failed to touch local-conf.yml file!"
 echo "Setup complete. You can now install the Dashy using the App Template."
