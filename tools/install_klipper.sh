d7f5ebb37211fd8f0424f1e04636be0393a55e7a
Robert Middleswarth <72569+rmiddle@users.noreply.github.com>
2022-11-14 09:50:17 -0500

    Add mailsail config files to the template

--- tools/install_klipper.sh
+++ tools/install_klipper.sh
@@ -5,6 +5,18 @@ function error {
   exit 1
 }
 
+function check_internet() {
+  printf "Checking if you are online..."
+  wget -q --spider http://github.com
+  if [ $? -eq 0 ]; then
+    echo "Online. Continuing."
+  else
+    error "Offline. Go connect to the internet then run the script again."
+  fi
+}
+
+check_internet
+
 echo "Creating directories..."
 sudo mkdir -p /portainer/Files/AppData/Config/klipper/klipper/ || error "Failed to create data directory for klipper / klipper!"
 sudo mkdir -p /portainer/Files/AppData/Config/klipper/klipper/logs || error "Failed to create data directory for klipper / klipper!"
@@ -27,6 +39,17 @@ then
     rm -rf '/portainer/Files/AppData/Config/klipper/moonraker/moonraker.conf'
 fi
 sudo touch /portainer/Files/AppData/Config/klipper/moonraker/moonraker.conf || error "Failed to touch moonraker.conf file!"
+
+if [ -d /portainer/Files/AppData/Config/klipper/mainsail/config.json ];
+then
+    echo "/portainer/Files/AppData/Config/klipper/mainsail/config.json is a directory removing"
+    rm -rf '/portainer/Files/AppData/Config/klipper/mainsail/config.json'
+fi
+
+if [ ! -f /portainer/Files/AppData/Config/klipper/mainsail/config.json ]; 
+then
+	sudo wget -O /portainer/Files/AppData/Config/klipper/mainsail/config.json https://raw.githubusercontent.com/pi-hosted/pi-hosted/master/configs/klipper/mainsail.json || error "Failed to download mainsail config file!"
+fi
 
 echo "Setting permissions..."
 sudo chown -R 1000:1000  /portainer/Files/AppData/Config/klipper/ || error "Failed to set permissions for klipper data!"
