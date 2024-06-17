9f017118311ce4d749e68d69c219f77a5e4b6e31
Robert Middleswarth <72569+rmiddle@users.noreply.github.com>
2024-06-17 12:36:00 -0400

    Update reinstall_portainer.sh
    Force purge and don't ask.

--- reinstall_portainer.sh
+++ reinstall_portainer.sh
@@ -30,7 +30,7 @@ sudo docker rm $portainer_pid || warning "Failed to remove portainer container!"
 sudo docker rmi $portainer_name || warning "Failed to remove/untag images from the container!"
 
 echo Pruning unused volume.  If asked answer yes.
-sudo docker volume prune
+sudo docker volume prune -f
 
 echo now doing a fresh install of portainer
 sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest --logo "https://pi-hosted.com/pi-hosted-logo.png" || error "Failed to execute newer version of Portainer!"
