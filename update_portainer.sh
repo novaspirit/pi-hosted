d0b50e1c8c0e5bec215f7312c332da9ebe72f2fa
Robert Middleswarth <72569+rmiddle@users.noreply.github.com>
2024-06-17 20:12:08 -0400

    Update update_portainer.sh

--- update_portainer.sh
+++ update_portainer.sh
@@ -23,4 +23,7 @@ portainer_name=`docker ps | grep portainer-ce | awk '{print $2}'`
 sudo docker stop $portainer_pid || error "Failed to stop portainer!"
 sudo docker rm $portainer_pid || error "Failed to remove portainer container!"
 sudo docker rmi $portainer_name || error "Failed to remove/untag images from the container!"
-sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest --logo "https://pi-hosted.com/pi-hosted-logo.png" || error "Failed to execute newer version of Portainer!"
+
+sudo mkdir -p /portainer/Files/AppData/Config/portainer || error "Failed to create the Portainer Config Folder"
+
+sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /portainer/Files/AppData/Config/portainer:/data portainer/portainer-ce:latest --logo "https://pi-hosted.com/pi-hosted-logo.png" || error "Failed to run Portainer docker image!"
