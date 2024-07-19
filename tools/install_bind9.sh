be0b328916ab217606efc733ff3a11bf70e13c3a
Robert Middleswarth <72569+rmiddle@users.noreply.github.com>
2024-07-19 17:08:20 -0400

    Update install_bind9.sh
    Fixed errors messages that were left over from the install script I copied from.

--- tools/install_bind9.sh
+++ tools/install_bind9.sh
@@ -19,8 +19,8 @@ check_internet
 
 echo "Creating directories..."
 sudo mkdir -p /portainer/Files/AppData/Config/bind9/ || error "Failed to create config directory!"
-sudo mkdir -p /portainer/Files/AppData/Config/bind9/cache/ || error "Failed to create data directory for Prometheus!"
-sudo mkdir -p /portainer/Files/AppData/Config/bind9/bind/ || error "Failed to create data directory for Grafana!"
+sudo mkdir -p /portainer/Files/AppData/Config/bind9/cache/ || error "Failed to create data directory for Cache!"
+sudo mkdir -p /portainer/Files/AppData/Config/bind9/bind/ || error "Failed to create data directory for Bind"
 echo "Downloading Prometheus config files if they don't exist"
 if [ -d /portainer/Files/AppData/Config/bind9/named.conf ];
 then
@@ -30,7 +30,7 @@ fi
 
 if [ ! -f /portainer/Files/AppData/Config/bind9/named.conf ]; 
 then
-	sudo wget -O /portainer/Files/AppData/Config/bind9/named.conf https://raw.githubusercontent.com/pi-hosted/pi-hosted/master/configs/bind9/named.conf || error "Failed to download prometheus.yml file!"
+	sudo wget -O /portainer/Files/AppData/Config/bind9/named.conf https://raw.githubusercontent.com/pi-hosted/pi-hosted/master/configs/bind9/named.conf || error "Failed to download named.conf file!"
 fi
 
 echo "Done You are ready to goto next step in the install document"
