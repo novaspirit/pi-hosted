2a8d199d9c27facb62169781691fd9b5b7855d41
Robert Middleswarth <72569+rmiddle@users.noreply.github.com>
2025-07-22 19:54:19 -0400

    Update install_portainer.sh

--- install_portainer.sh
+++ install_portainer.sh
@@ -20,5 +20,5 @@ check_internet
 sudo mkdir -p /portainer/Files/AppData/Config/portainer || error "Failed to create the Portainer Config Folder"
 
 sudo docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
-sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /portainer/Files/AppData/Config/portainer:/data portainer/portainer-ce:latest --logo "https://pi-hosted.com/pi-hosted-logo.png" || error "Failed to run Portainer docker image!"
+sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /portainer/Files/AppData/Config/portainer:/data portainer/portainer-ce:latest --logo "https://raw.githubusercontent.com/pi-hosted/pi-hosted/master/images/pi-hosted-logo.png" || error "Failed to run Portainer docker image!"
 
